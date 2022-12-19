0297b6fd2adba2b8dd8837a3fe30db78a930b6e5
hoangquach <quachhoangqb@gmail.com>
2022-12-19 19:00:03 +0700

    Add filter item

--- notificationtracker/ApplicationListActivity.cs
+++ notificationtracker/ApplicationListActivity.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 using Android.App;
@@ -12,6 +14,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Newtonsoft.Json;
+using ReactiveUI;
 using Xamarin.Essentials;
 
 namespace notificationtracker
@@ -20,10 +23,15 @@ namespace notificationtracker
     public class ApplicationListActivity : Activity
     {
         private ListView listView;
-        private List<ApplicationModel> applications;
+        private List<ApplicationModel> applications, originalApps;
         private ApplicationListAdapter applicationListAdapter;
 
         private string google_default_sms_app = "com.google.android.apps.messaging";
+
+        private Button bttAdd;
+        private EditText edtPackage;
+
+        private BehaviorSubject<string> _filterPackage = new BehaviorSubject<string>(string.Empty);
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,13 +39,77 @@ namespace notificationtracker
             SetContentView(Resource.Layout.activity_application_list);
 
             listView = FindViewById<ListView>(Resource.Id.listView);
+            bttAdd = FindViewById<Button>(Resource.Id.bttAdd);
+            edtPackage = FindViewById<EditText>(Resource.Id.edtPackage);
+
+
             applications = new List<ApplicationModel>();
             applicationListAdapter = new ApplicationListAdapter(this, applications);
             listView.Adapter = applicationListAdapter;
 
             listView.ItemClick += ListView_ItemClick;
+            bttAdd.Click += BttAdd_Click;
+
+            edtPackage.TextChanged += EdtPackage_TextChanged;
 
             FetchApplication();
+
+            var observableFiltering = _filterPackage
+                .Skip(1)
+                .Throttle(TimeSpan.FromMilliseconds(1000))
+                .SubscribeOn(RxApp.TaskpoolScheduler)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(keyword =>
+                {
+                    var result = new List<ApplicationModel>();
+                    if (!string.IsNullOrEmpty(keyword))
+                    {
+                        var filterd = originalApps.Where(x => x.AppPackage.Contains(keyword) || x.AppName.Contains(keyword));
+                        if (filterd != null)
+                            result.AddRange(filterd);
+                    }
+                    else
+                    {
+                        result.AddRange(originalApps);
+                    }
+                    applications = new List<ApplicationModel>(result);
+                    applicationListAdapter = new ApplicationListAdapter(this, applications);
+                    listView.Adapter = applicationListAdapter;
+                });
+        }
+
+        private void BttAdd_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(edtPackage.Text?.ToString()))
+            {
+                return;
+            }
+
+            Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            AlertDialog alert = dialog.Create();
+            alert.SetTitle("Add package");
+            alert.SetMessage($"Add app => {edtPackage.Text} ??");
+            alert.SetButton("OK", (c, ev) =>
+            {
+                var newApp = new ApplicationModel()
+                {
+                    AppName = "new_app",
+                    AppPackage = edtPackage.Text,
+                    IsSelected = true
+                };
+
+                applications.Insert(0, newApp);
+                originalApps.Insert(0, newApp);
+                listView.Adapter = applicationListAdapter;
+
+                SaveSetting();
+            });
+            alert.Show();
+        }
+
+        private void EdtPackage_TextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            _filterPackage.OnNext(e.Text?.ToString());
         }
 
         private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
@@ -45,6 +117,10 @@ namespace notificationtracker
             var item = applications[e.Position];
             item.IsSelected = !item.IsSelected;
             applicationListAdapter.NotifyDataSetChanged();
+
+            var originItem = originalApps?.Where(x => x.AppPackage == item.AppPackage)?.FirstOrDefault();
+            if(originItem != null)
+                originItem.IsSelected = item.IsSelected;
 
             SaveSetting();
         }
@@ -75,7 +151,6 @@ namespace notificationtracker
                         AppPackage = item.ApplicationInfo.PackageName,
                         IsSelected = appSetted.Contains(item.ApplicationInfo.PackageName)
                     });
-
                 }
 
                 if(result.Where(x => x.AppPackage.Contains(google_default_sms_app))?.Any() == false)
@@ -88,11 +163,26 @@ namespace notificationtracker
                     });
                 }
 
+                var allApps = result.Select(x => x.AppPackage);
+                var savedButNotIn = appSetted.Where(x => !allApps.Any(a => x == a));
+                foreach (var item in savedButNotIn)
+                {
+                    result.Add(new ApplicationModel()
+                    {
+                        AppName = "Save_",
+                        AppPackage = item,
+                        IsSelected = true
+                    });
+                }
+
                 var sorted = result.OrderBy(x => x.AppName);
+
+
 
                 MainThread.BeginInvokeOnMainThread(() =>
                 {
                     applications.AddRange(sorted);
+                    originalApps = new List<ApplicationModel>(sorted);
                     applicationListAdapter.NotifyDataSetChanged();
                 });
             });
